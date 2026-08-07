9f674360c38e3c786f84d04d45ccd78b04a23134
flyshieny7-hue <flyshieny7@gmail.com>
2026-08-07 18:54:44 +0300

    Delete AppleWalletClone/Views/SendRequest directory

--- AppleWalletClone/Views/SendRequest/ContactPickerView.swift
+++
@@ -1,8 +0,0 @@
-import SwiftUI
-
-struct ContactPickerView: View {
-    var body: some View {
-        Text("Contact Picker")
-            .foregroundColor(.white)
-    }
-}
--- AppleWalletClone/Views/SendRequest/SendRequestView.swift
+++
@@ -1,8 +0,0 @@
-import SwiftUI
-
-struct SendRequestView: View {
-    var body: some View {
-        Text("Send or Request")
-            .foregroundColor(.white)
-    }
-}
